a2fd2b108338a533f6ca43074150ba6319170ec5
pavelFilo <pvlfilo@gmail.com>
2022-10-22 22:05:18 +0200

    feat: heatmap

--- dapper-app/src/Page/Page.tsx
+++ dapper-app/src/Page/Page.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from 'react'
-import { generateMap, generateRoutes } from '../api/endpoints'
+import { generateMap, generateRoutes, weatherForecast } from '../api/endpoints'
 import { SplashScreenLoader } from '../components/SplashScreenLoader'
 import { IRoute, Map } from './partials/Map'
 import {
@@ -8,12 +8,15 @@ import {
 } from './partials/Modifications'
 import { IRoutesFormValues, Routes } from './partials/Routes'
 import { FormikHelpers } from 'formik'
+import Specifition from '../assets/form-24-regular.svg'
 
 export const Page = () => {
   const [routeData, setRouteData] = useState<
     { routes: IRoute[]; summary: any } | undefined
   >()
   const [significantPoints, setSignificantPoints] = useState()
+  const [weather, setWeather] = useState()
+  const [isWeatherShown, setIsWeatherShown] = useState(false)
 
   const onFetchRoutes = useCallback(
     async (
@@ -52,6 +55,12 @@ export const Page = () => {
     [generateMap]
   )
 
+  const onFetchWeather = useCallback(async () => {
+    const res = await weatherForecast()
+
+    setWeather(res?.content?.[0])
+  }, [weatherForecast])
+
   return (
     <>
       {false ? (
@@ -61,6 +70,7 @@ export const Page = () => {
           <Modifications onFetchSignificantPoints={onFetchSignificantPoints} />
 
           <Map
+            weather={weather}
             routes={routeData?.routes}
             significantPoints={significantPoints}
           />
@@ -70,6 +80,18 @@ export const Page = () => {
             hasSignificantPoints={!!significantPoints}
             onFetchRoutes={onFetchRoutes}
           />
+
+          <button
+            className={`z-500 absolute left-4 bottom-5 p-2 rounded-3xl transition-opacity bg-dark ${
+              isWeatherShown ? 'opacity-0' : 'opacity-1'
+            }`}
+            onClick={() => {
+              setIsWeatherShown(true)
+              onFetchWeather()
+            }}
+          >
+            <img className="specifition" src={Specifition} alt="" />
+          </button>
         </div>
       )}
     </>
