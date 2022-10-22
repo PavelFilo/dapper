aca355734240562778f0df6246d77df33accb2c5
Peter Kulcsar Szabo <peter@nextretreat.com>
2022-10-22 20:06:17 +0200

    added additional layers of data

--- api/services/constants.js
+++ api/services/constants.js
@@ -5,11 +5,26 @@ const MIN_SNOW_VALUE = 0.000001
 
 const SIGNIFICANT_POINTS_PATH = './data/significantPoints.json'
 
+const LAYER_PROPERTIES = {
+  IS_TROLLEY: 'isTrolley',
+  CLASS: 'class',
+  IS_CRITICAL_PT: 'isCriticalPT',
+  IS_SCHOOL_NEARBY: 'isSchoolNearby',
+  IS_HOSPITAL_NEARBY: 'isHospitalNearby',
+  IS_POLICE_NEARBY: 'isPoliceNearby',
+  IS_FIRE_DEP_NEARBY: 'isFireDepNearby',
+  IS_UNIVERSITY_NEARBY: 'isUniversityNearby',
+}
+
 const DEFAULT_WEIGHTS = {
-  class: 5,
-  isTrolley: 2,
-  isCriticalPT: 5,
-  isSchoolNearby: 1,
+  [LAYER_PROPERTIES.CLASS]: 5,
+  [LAYER_PROPERTIES.IS_TROLLEY]: 2,
+  [LAYER_PROPERTIES.IS_CRITICAL_PT]: 5,
+  [LAYER_PROPERTIES.IS_SCHOOL_NEARBY]: 1,
+  [LAYER_PROPERTIES.IS_HOSPITAL_NEARBY]: 1,
+  [LAYER_PROPERTIES.IS_POLICE_NEARBY]: 1,
+  [LAYER_PROPERTIES.IS_FIRE_DEP_NEARBY]: 1,
+  [LAYER_PROPERTIES.IS_UNIVERSITY_NEARBY]: 1,
 }
 
 module.exports = {
@@ -18,4 +33,5 @@ module.exports = {
   MIN_SNOW_VALUE,
   SIGNIFICANT_POINTS_PATH,
   DEFAULT_WEIGHTS,
+  LAYER_PROPERTIES,
 }
